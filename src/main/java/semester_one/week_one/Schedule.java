4e1811996786c7f5300d765ef77e90fc3c1ee33e
Michael Muzzarelli <mjmuzzarelli@icloud.com>
2020-08-26 22:44:03 -0500

    Complete 8/28 program assignment, Schedule

---
+++ src/main/java/semester_one/week_one/Schedule.java
@@ -0,0 +1,20 @@
+package semester_one.week_one;
+
+/**
+ * Name:            Michael Muzzarelli
+ * Date:            8/26/2020
+ * Description:     Prints my course schedule.
+ */
+public class Schedule {
+
+    public static void main(String[] args) {
+        System.out.println("COURSE SCHEDULE:");
+        System.out.println("Name:      Michael Muzzarelli");
+        System.out.println("Email:     michael.muzzarelli@student.swic.edu");
+        System.out.println("Semester:  Fall 2020");
+        System.out.println(" ");
+        System.out.println("COURSE         DAY        TIME          ROOM       INSTRUCTOR");
+        System.out.println("MATH 171-001   MoTuThFr   9:00A-9:50A   MC2183     Collins");
+        System.out.println("PSYC 225-IN1   Internet   Internet      Internet   Hunter");
+    }
+}
